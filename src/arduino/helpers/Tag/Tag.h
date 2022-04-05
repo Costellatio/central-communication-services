80fd2c980106d677d57b685b19d835d92a7c8fad
Stuksi <stoyan.konstantinov.grozdanov@gmail.com>
2022-04-05 17:21:59 +0300

    poc finalizing

---
+++ src/arduino/helpers/Tag/Tag.h
@@ -0,0 +1,11 @@
+#ifndef TAG_H
+#define TAG_H
+
+#include <Arduino.h>
+
+struct Tag {
+  String name;
+  String value;
+};
+
+#endif
---
+++ src/arduino/sensors/TMP_38GZ/TMP_38GZ.h
@@ -0,0 +1,8 @@
+#ifndef TMP_38GZ_H
+#define TMP_38GZ_H
+
+#include <Arduino.h>
+
+String temperature_sensor_serial_data();
+
+#endif
