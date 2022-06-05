89d83c154389be1fc8bb6773f95346af1099c34e
stuksi <stoyan.konstantinov.grozdanov@gmail.com>
2022-06-05 14:24:33 +0300

    Clean up the main ino file

--- src/arduino/main.ino
+++ src/arduino/main.ino
@@ -1,23 +1,18 @@
-#include <WS_BME_280.h>
-#include <arduino-timer.h>
-#include <common.h>
+#include <SensorToggles.h>
+#include <Timer.h>
 
-Adafruit_BME280 bme;
-Timer<> ws_timer = timer_create_default();
+Timer<> timer;
+
+#ifdef BME280_ENABLED
+  #include <BME280.h>
+
+  BME280 _(timer);
+#endif
 
 void setup() {
-    Serial.begin(9600);
-    while(!Serial);
-
-    bool status = bme.begin(0x76);
-
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!\n");
-        while (1) delay(10);
-    }
-  ws_timer.every(WS_TIME_PERIOD_SECONDS * 1000UL, print_weather_station_data, &bme);
+  Serial.begin(9600);
 }
 
 void loop() {
-  ws_timer.tick();
+  timer.tick();
 }
