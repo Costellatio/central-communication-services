d64d96ba70a3799766083c42b6c291d8b9afe7a8
stuksi <stoyan.konstantinov.grozdanov@gmail.com>
2022-06-14 20:22:23 +0300

    mpu activation

--- src/arduino/sensors/MPU6050/MPU6050.cpp
+++ src/arduino/sensors/MPU6050/MPU6050.cpp
@@ -4,14 +4,20 @@
 #include <SensorIds.h>
 #include <SensorInputs.h>
 
-#define TICK_INTERVAL 1 * 1000UL
+#define TICK_INTERVAL 0.1 * 1000UL
 #define ACCELEROMETER_REGISTER 0x3B
+#define POWER_MANAGEMENT_REGISTER 0x6B
 #define WIRE_REQUEST_SIZE 6
 #define SERIALIZER_PROPERTIES 3
 
 MPU6050::MPU6050(Timer<>& timer) : Sensor(timer) {}
 
 void MPU6050::run() {
+  Wire.beginTransmission(MPU6050_I2C_ADDRESS);
+  Wire.write(POWER_MANAGEMENT_REGISTER);
+  Wire.write(0x00);
+  Wire.endTransmission();
+
   timer().every(TICK_INTERVAL, []() -> bool {
     Wire.beginTransmission(MPU6050_I2C_ADDRESS);
     Wire.write(ACCELEROMETER_REGISTER);
