9d9b2129de969550bcaae802b1075feccc721894
stuksi <stoyan.konstantinov.grozdanov@gmail.com>
2022-07-16 18:04:59 +0300

    Added eslint and sockeio config

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
+import socketio from 'socket.io';
+import http from 'http';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
+const socket = new socketio.Server(server);
 
-app.get('/', (req, res) => {
-  res.send('Hellos world');
+socket.on('connection', (connection) => {
+  console.log(`hekk hello ${connection}`);
 });
 
-app.listen(3030, () => {
+server.listen(3030, () => {
   console.log('App listenings on port 3030');
 });
